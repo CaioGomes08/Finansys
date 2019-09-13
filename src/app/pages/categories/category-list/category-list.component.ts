70afd7a86ed4ec3e05a3002c1dfc21552be143e8
Caio Gomes <caiovini-2015@bol.com.br>
2019-09-12 21:02:44 -0300

    Listando e excluido categoria

--- src/app/pages/categories/category-list/category-list.component.ts
+++ src/app/pages/categories/category-list/category-list.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Category } from '../shared/category.model';
+import { CategoryService } from '../shared/category.service';
+
 @Component({
   selector: 'app-category-list',
   templateUrl: './category-list.component.html',
-  styleUrls: ['./category-list.component.css']
+  styleUrls: ['./category-list.component.css'],
 })
 export class CategoryListComponent implements OnInit {
 
-  constructor() { }
+  categories: Category[] = [];
+
+  constructor(private categoryService: CategoryService) { }
 
   ngOnInit() {
+    this.categoryService.getAll()
+      .subscribe(categories => {
+        this.categories = categories;
+      }, error => alert('Erro ao carregar a lista')
+      );
   }
 
+  deleteCategory(category) {
+    const mustDelete = confirm('Deseja realmente excluir esse item ?');
+  
+    if (mustDelete) {
+      this.categoryService.delete(category.id)
+      .subscribe(() => this.categories = this.categories.filter(e => e !== category),
+        () => alert('Erro ao tentar excluir'));
+    }
+  }
 }
