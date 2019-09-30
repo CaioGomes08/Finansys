51b006dfa8ac07d1eb8aacd2da847b984f63d22b
Caio Gomes <caiovini-2015@bol.com.br>
2019-09-30 20:46:15 -0300

    Cadastrando categorias

--- src/app/pages/categories/category-form/category-form.component.ts
+++ src/app/pages/categories/category-form/category-form.component.ts
@@ -4,8 +4,9 @@ import { ActivatedRoute, Router } from '@angular/router';
 
 import { Category } from '../shared/category.model';
 import { CategoryService } from '../shared/category.service';
-import { switchMap } from "rxjs/operators";
-import { toastr } from 'toastr';
+import { switchMap } from 'rxjs/operators';
+// tslint:disable-next-line: import-spacing
+import toastr from 'toastr';
 
 @Component({
   selector: 'app-category-form',
@@ -18,7 +19,7 @@ export class CategoryFormComponent implements OnInit, AfterContentChecked {
   currentAction: string;
   categoryForm: FormGroup;
   pageTitle: string;
-  serverErrorMessages: string[] = [];
+  serverErrorMessages: string[] = null;
   submittingForm = false;
   category: Category = new Category();
 
@@ -26,7 +27,7 @@ export class CategoryFormComponent implements OnInit, AfterContentChecked {
     private categoryService: CategoryService,
     private route: ActivatedRoute,
     private router: Router,
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
   ) { }
 
   ngOnInit() {
@@ -37,6 +38,15 @@ export class CategoryFormComponent implements OnInit, AfterContentChecked {
 
   ngAfterContentChecked(): void {
     this.setPageTitle();
+  }
+
+  submitForm() {
+    this.submittingForm = true;
+    if (this.currentAction === 'new') {
+      this.createCategory();
+    } else {
+      this.updateCategory();
+    }
   }
 
   // PRIVATES METHODS
@@ -61,13 +71,13 @@ export class CategoryFormComponent implements OnInit, AfterContentChecked {
       this.route.paramMap.pipe( // recupera os parametros passados pela rota
         switchMap(params => this.categoryService.getById(+params.get('id'))) // mapeando os valores retornados do service
       )
-      .subscribe(
-        (category) => {
-          this.category = category;
-          this.categoryForm.patchValue(this.category); // Setando os valores retornados no formulário
-        },
-        (error) => alert('Ocorreu um erro no servidor, tente mais tarde')
-      );
+        .subscribe(
+          (category) => {
+            this.category = category;
+            this.categoryForm.patchValue(this.category); // Setando os valores retornados no formulário
+          },
+          (error) => alert('Ocorreu um erro no servidor, tente mais tarde')
+        );
     }
   }
 
@@ -76,7 +86,51 @@ export class CategoryFormComponent implements OnInit, AfterContentChecked {
       const categoryName = this.category.name || '';
       this.pageTitle = `Editando Categoria - ${categoryName}`;
     } else {
-      this.pageTitle = 'Criando categoria';
+      this.pageTitle = 'Cadastro de categoria';
+    }
+  }
+
+  private createCategory() {
+    const category: Category = Object.assign(new Category(), this.categoryForm.value);
+
+    this.categoryService.create(category)
+      .subscribe(
+        // tslint:disable-next-line: no-shadowed-variable
+        category => this.actionsForSuccess(category),
+        error => this.actionsForError(error)
+      );
+  }
+
+  private updateCategory() {
+    const category: Category = Object.assign(new Category(), this.categoryForm.value);
+
+    this.categoryService.update(category)
+      .subscribe(
+        // tslint:disable-next-line: no-shadowed-variable
+        category => this.actionsForSuccess(category),
+        error => this.actionsForError(error)
+      );
+  }
+
+  private actionsForSuccess(category: Category) {
+    toastr.success('Solicitação processada com sucesso!');
+
+    // redirecionando e recarregando o component
+    this.router.navigateByUrl('categories', { skipLocationChange: true })
+      .then(
+        () => this.router.navigate(['categories', category.id, 'edit'])
+      );
+  }
+
+  private actionsForError(error) {
+    toastr.error('Erro ao processar solicitação!');
+
+    this.submittingForm = false;
+
+    if (error.status === 422) {
+      this.serverErrorMessages = JSON.parse(error._body).errors;
+    } else {
+      this.serverErrorMessages = ['Falha na comunicação com o servidor. Por favor, tente mais tarde'];
     }
   }
 }
